604e39be42d64d6af486f12d65124de54b571124
dhiyo7 <dhiyoreksa12@gmail.com>
2020-12-21 14:02:09 +0700

    Fixing Bug merger

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Home from '../pages/home'
-import AddRecipe from '../pages/addRecipe/addRecipe'
+import Home from "../pages/Home";
+import Login from "../pages/auth/Login"
+import Register from "../pages/auth/Register"
+import ForgotPass from "../pages/auth/ForgotPass"
+import AddRecipe from "../pages/addRecipe/addRecipe";
 
 const Router = () => {
-	return (
-		<BrowserRouter>
-			<Route exact path="/" component={Home} />
-			<Route path="/addRecipe" component={AddRecipe} />
-		</BrowserRouter>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/forgot-password" component={ForgotPass} />
+      <Route path="/addRecipe" component={AddRecipe} />
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
