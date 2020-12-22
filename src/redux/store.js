2c2f4c39d9401700f0e52be5f7579bb1478c9d65
woobwoofwolf <imbakakak@gmail.com>
2020-12-23 03:18:40 +0700

    create private route

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { createLogger } from "redux-logger";
+// import promiseMiddleware from "redux-promise-middleware";
+
+// import reducers from "./reducers";
+
+// const logger = createLogger();
+
+// const enhancers = applyMiddleware(promiseMiddleware, logger);
+
+// const reduxStore = createStore(reducers, composeWithDevTools(enhancers));
+
+// export default reduxStore;
