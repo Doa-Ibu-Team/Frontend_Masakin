c7bfa368c0c955a6874b55cc6bc6e162af6171ad
woobwoofwolf <imbakakak@gmail.com>
2020-12-29 19:36:22 +0700

    integrating otp features into frontend

--- src/components/auth/CodeReset.jsx
+++ src/components/auth/CodeReset.jsx
@@ -6,6 +6,9 @@ import { Redirect } from 'react-router-dom';
 import swal from 'sweetalert'
 import { setCodeReset, setCodeResetFalse } from '../../redux/actions/Auth'
 import { connect } from 'react-redux'
+import axios from 'axios'
+
+const qs = require('querystring')
 
 const base_url = process.env.REACT_APP_BASE_URL
 
@@ -14,100 +17,115 @@ class CodeReset extends Component {
     isCodeReset: false
   }
   handleSubmit = (e) => {
-    const {dispatch} = this.props;
-    const data = {
-        otp: this.otp,
+    const { dispatch } = this.props;
+    const dataOtp = {
+      email: localStorage.getItem('email'),
+      otp: this.otp
     }
+    console.log(dataOtp)
     e.preventDefault()
-    if(data.otp !== localStorage.getItem("otp")) {
-      dispatch(setCodeResetFalse())
-      swal("Code OTP is Wrong!")
-    }
-    else {
-      dispatch(setCodeReset());
-      localStorage.removeItem("otp")
-      swal("Code OTP True")
-    }
-}
-    render() {
-      console.log(localStorage)
-      const { auth } = this.props;
+    axios.get(base_url + `/auth/otp/${dataOtp.email}/${dataOtp.otp}`)
+      .then(({ data }) => {
+        localStorage.removeItem('email')
+        localStorage.setItem('token', data.token)
+        swal(data.message)
+        this.setState({
+          isCodeReset: true
+        })
+        dispatch(setCodeReset())
+      }).catch((error) => {
+        dispatch(setCodeResetFalse())
+        console.log(error)
+      })
+    // if(data.otp !== localStorage.getItem("otp")) {
+    //   dispatch(setCodeResetFalse())
+    //   swal("Code OTP is Wrong!")
+    // }
+    // else {
+    //   dispatch(setCodeReset());
+    //   localStorage.removeItem("otp")
+    //   swal("Code OTP True")
+    // }
+  }
+  render() {
+    console.log(localStorage)
+    const { auth } = this.props;
     return (
-    <>
-      <div className="main">
-      {auth.isCodeReset && <Redirect to="/reset-password" />}
-        <div className="left-content">
-          <div className="rectangle-overlay"></div>
-          <div className="logo-login">
-            <img
-              className="img-login"
-              src={Logo}
-              alt="ini logo pak"
-            />
-            <h3 className="text-logo mt-4 ml-4">Mama recipe</h3>
-          </div>
-          <div className="box-form-left">
-          <Form className="my-form" style={{marginTop: "550px"}}>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Code 6 Digit</Form.Label>
-                <Form.Control
-                  className="shadow"
-                  type="text"
-                  name="code_reset"
-                  placeholder=""
-                  // onChange={(e) => (this.otp = e.target.value)}
-                />
-              </Form.Group>
+      <>
+        <div className="main">
+          {auth.isCodeReset && <Redirect to="/reset-password" />}
+          <div className="left-content">
+            <div className="rectangle-overlay"></div>
+            <div className="logo-login">
+              <img
+                className="img-login"
+                src={Logo}
+                alt="ini logo pak"
+              />
+              <h3 className="text-logo mt-4 ml-4">Mama recipe</h3>
+            </div>
+            <div className="box-form-left">
+              <Form className="my-form" style={{ marginTop: "550px" }}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Code 6 Digit</Form.Label>
+                  <Form.Control
+                    className="shadow"
+                    type="text"
+                    name="code_reset"
+                    placeholder=""
+                    onChange={(e) => (this.otp = e.target.value)}
+                  />
+                </Form.Group>
                 <Button
-                    type="submit"
-                    style={{
+                  type="submit"
+                  style={{
                     width: "100%",
                     backgroundColor: "#EFC81A",
                     padding: "15px",
-                    }}
-                    // onClick={this.handleSubmit}
+                  }}
+                // onClick={this.handleSubmit}
                 >
-                    Reset Password
+                  Reset Password
                 </Button>
-            </Form>
+              </Form>
+            </div>
           </div>
-        </div>
-        <div className="right-content">
-          <div className="box-form-right-side">
-            <Form className="my-form-right w-75" style={{marginTop: "340px"}}>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Code 6 Digit</Form.Label>
-                <Form.Control
-                  className="shadow"
-                  type="text"
-                  placeholder=""
-                  onChange={(e) => (this.otp = e.target.value)}
-                />
-              </Form.Group>
+          <div className="right-content">
+            <div className="box-form-right-side">
+              <Form className="my-form-right w-75" style={{ marginTop: "340px" }}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Code 6 Digit</Form.Label>
+                  <Form.Control
+                    className="shadow"
+                    type="text"
+                    placeholder=""
+                    onChange={(e) => (this.otp = e.target.value)}
+                  />
+                </Form.Group>
                 <Button
-                    type="submit"
-                    style={{
+                  type="submit"
+                  style={{
                     width: "100%",
                     backgroundColor: "#EFC81A",
                     padding: "15px",
-                    }}
-                    onClick={this.handleSubmit}
+                  }}
+                  onClick={this.handleSubmit}
                 >
-            
-                    Reset Password
+
+                  Reset Password
                 </Button>
-            </Form>
+              </Form>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-        )
-    }
+      </>
+    )
+  }
 }
 const mapStateToProps = ({ auth, newState }) => {
   return {
-      auth,
-      newState
+    auth,
+    newState
   };
 };
 export default connect(mapStateToProps)(CodeReset)
